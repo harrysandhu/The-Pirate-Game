87d063d570b2e27268a3530438ac5f04d4e21396
MAC <Harry@apples-mac-mini-3.local>
2015-10-10 14:25:15 +0530

    Initial Commit

---
+++ The Pirate Game/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  The Pirate Game
+//
+//  Created by Harry on 10/10/15.
+//  Copyright © 2015 Harmandeep Singh Sandhu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
